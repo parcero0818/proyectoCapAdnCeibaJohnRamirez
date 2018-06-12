464e074b3b32683ef92e6474aebb8b32898f7027
John Edward Ramírez Arango - Ceiba Software <john.ramirez@ceiba.com.co>
2018-06-12 15:05:49 -0500

    primer commit

---
+++ proyecto1/proyecto1/src/main/java/co/ceiba/domain/Person.java
@@ -0,0 +1,28 @@
+package co.ceiba.domain;
+
+public class Person {
+	String name;
+	String lastName;
+
+	public Person(String name, String lastName) {
+		this.name = name;
+		this.lastName = lastName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
